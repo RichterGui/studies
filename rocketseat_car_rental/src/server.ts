d8ef118c33c1ed93d0518836809bb23463f78c3f
Guilherme <richtergui97@gmail.com>
2022-11-12 10:35:55 -0300

    init

--- rocketseat_car_rental/src/server.ts
+++ rocketseat_car_rental/src/server.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import { categoriesRoutes } from "./routes/catefories.routes";
 
 const app = express();
+
 app.use(express.json());
 
+app.use("/categories", categoriesRoutes);
+
 const PORT = 3333;
-// eslint-disable-next-line no-console
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));

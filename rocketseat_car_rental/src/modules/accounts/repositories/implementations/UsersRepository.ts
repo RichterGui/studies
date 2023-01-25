38fe6fedd7e2215eedacb9c5aeb22aad5edfc204
Guilherme <richtergui97@gmail.com>
2023-01-25 14:00:24 -0300

    user-entity

---
+++ rocketseat_car_rental/src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,35 @@
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { IUsersRepository } from "../IUsersRepository";
+import { User } from "../../entities/User";
+import { getRepository, Repository } from "typeorm";
+
+class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = getRepository(User);
+  }
+  async findByEmail(email: string): Promise<User> {
+    const user = await this.repository.findOne({ email });
+    return user;
+  }
+  async create({
+    name,
+    username,
+    email,
+    driver_license,
+    password,
+  }: ICreateUserDTO): Promise<void> {
+    const user = this.repository.create({
+      name,
+      username,
+      email,
+      driver_license,
+      password,
+    });
+
+    await this.repository.save(user);
+  }
+}
+
+export { UsersRepository };

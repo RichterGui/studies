a0de2a413fa0d46950939c0cc4db6e039a554df1
Guilherme <richtergui97@gmail.com>
2023-01-27 14:13:18 -0300

    avatar included

--- rocketseat_car_rental/src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ rocketseat_car_rental/src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -3,6 +3,7 @@ import { IUsersRepository } from "../../repositories/IUsersRepository";
 import { sign } from "jsonwebtoken";
 
 import { compare } from "bcrypt";
+import { AppError } from "../../../../errors/AppError";
 
 interface IRequest {
   email: string;
@@ -27,13 +28,13 @@ class AuthenticateUserUseCase {
     const user = await this.usersRepository.findByEmail(email);
 
     if (!user) {
-      throw new Error("Incorrect credentials");
+      throw new AppError("Incorrect credentials");
     }
 
     const passwordMatch = await compare(password, user.password);
 
     if (!passwordMatch) {
-      throw new Error("Incorrect credentials");
+      throw new AppError("Incorrect credentials");
     }
 
     const token = sign({}, "ba5cd5d53c72e4fdc63a94d7a140b321", {

54e310efaa7950851d5bd5ef62ebcca55fbcbd19
Guilherme <richtergui97@gmail.com>
2024-04-10 02:15:09 -0300

    java c started

---
+++ java-rs/fundamentos-java/HelloWorld.java
@@ -0,0 +1,20 @@
+
+public class HelloWorld {
+
+
+  public static void main(String[] args) {
+    short a = 1;
+    int a1 = 3;
+    long b = 4;
+
+
+
+    while(b < 10) {
+      System.out.println(b);
+      b +=1;
+    }
+
+  }
+ 
+  
+}
